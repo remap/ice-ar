5469d38ce3bf5cad3ef5f0b10dd78a817e4efa90
peetonn <peter@remap.ucla.edu>
2017-11-07 16:29:36 -0800

    added classifier to OpenFace

--- edge/openface/main.py
+++ edge/openface/main.py
@@ -16,10 +16,29 @@ import _nanomsg_ctypes as nnc
 import struct
 import datetime
 import json
+import pandas
+import sklearn
+from sklearn import preprocessing
+from sklearn import svm
 
 dlibModelPath = '/home/peter/openface/models/dlib/shape_predictor_68_face_landmarks.dat'
+torchModelPath = '/home/peter/openface/models/openface/nn4.small2.v1.t7'
 dlibObject = None
 dumpSocket = None
+labelEncoder = None
+svc = None
+torchNn = None
+
+def initClassifier(labelsFile, repsFile):
+	global svc, labelEncoder
+	reps = pandas.read_csv(repsFile).as_matrix()
+	labelsCsv = pandas.read_csv(labelsFile).as_matrix()
+	labels = labelsCsv[:,0]
+	# labels = [x for i,x in enumerate(tmp) if x not in tmp[0:i]]
+	labelEncoder = preprocessing.LabelEncoder().fit(labels)
+	transformedLabels = labelEncoder.transform(labels)
+	svc = svm.SVC(C=1, kernel="rbf", probability=True, gamma=2)
+	svc.fit(reps, transformedLabels)
 
 # reads a frame from nanomsg socket
 def readFrame(socket, frameSize):
@@ -46,9 +65,22 @@ def dumpAnnotations(frameNo, annotations):
 	# print(" > will send this "+s)
 	dumpSocket.send(s)
 
+def runClassifier(img, annotation, rect):
+	global dlibObject, torchNn, svc, labelEncoder
+	alignedImg = dlibObject.align(96, img, bb=rect, landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
+	features = torchNn.forward(alignedImg)
+	# need to reshape features, as svc object expects 2d array, while features is 1d array
+	predictions = svc.predict_proba(features.reshap(1,-1)).ravel()
+	maxLikelihood = np.argmax(predictions)
+	label = labelEncoder.inverse_transform(maxLikelihood)
+	if maxLikelihood > 0.5:
+		annotation['prob'] = maxLikelihood
+		annotation['label'] = label
+	return annotation
+
 counter = 0
 def processFrames(socket, frameWidth, frameHeight):
-	global dlibObject, counter
+	global dlibObject, counter, svc
 	frameSize = frameWidth*frameHeight*4 # assuming ARGB
 
 	while True:
@@ -75,6 +107,8 @@ def processFrames(socket, frameWidth, frameHeight):
 				facesArray = []
 				for r in rects:
 					faceAnnotation = getAnnotationFromRect(r, frameWidth, frameHeight)
+					# run classifier, if probabilirt less than 50%, ignore classifier's data
+					faceAnnotation = runClassifier(imgBGR, faceAnnotation, r)
 					facesArray.append(faceAnnotation)
 				dumpAnnotations(frameNumber, facesArray)
 			else:
@@ -95,13 +129,13 @@ def processFrames(socket, frameWidth, frameHeight):
 
 #******************************************************************************
 def usage():
-	print ""
+	print "\t"+sys.argv[0]+" [--frame-width=, --frame-height=] --labels=<labels file>, --reps=<reps file> [pipe file]"
 	print "usage: "
 
 def main():
-	global dlibObject, dumpSocket
+	global dlibObject, dumpSocket, torchNn, torchModelPath
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "w:h:", ["frame-width=", "frame-height="])
+		opts, args = getopt.getopt(sys.argv[1:], "w:h:", ["frame-width=", "frame-height=", "labels=", "reps="])
 	except getopt.GetoptError as err:
 		print str(err)
 		usage()
@@ -109,14 +143,25 @@ def main():
 	frameWidth=320
 	frameHeight=240
 	pipeName = "/tmp/mtcamera.320x240"
+	repsFile = None
+	labelsFile = None
 
 	for o,a in opts:
 		if o in ("-w", "--frame-width"):
 			frameWidth = int(a)
 		elif o in ("-h", "--frame-height"):
 			frameHeight = int(a)
+		elif o in ("--labels"):
+			labelsFile = a
+		elif o in ("--reps"):
+			repsFile = a
 		else:
 			assert False, "unhandled option "+ o
+
+	if not repsFile or not labelsFile:
+		print "please provide reps and labels file"
+		usage()
+		exit(2)
 
 	if len(args) == 1:
 		pipeName = args[0]+"."+str(frameWidth)+"x"+str(frameHeight)
@@ -131,6 +176,15 @@ def main():
 	print(" > initializing OpenFace...")
 	dlibObject=openface.AlignDlib(dlibModelPath)
 	print(" > ...done.")
+	
+	print(" > initializing classifier...")
+	initClassifier(labelsFile, repsFile)
+	print(" > ...done.")
+
+	print(" > initializing torch feature extractor...")
+	torchNn = openface.TorchNeuralNet(torchModelPath, imgDim=96)
+	print(" > ...done.")
+
 	print(" > processing frames of size "+str(frameWidth)+"x"+str(frameHeight)+" from "+pipeName)
 	processFrames(s, frameWidth, frameHeight)
 
