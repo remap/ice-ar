57e344e6a6c647d06cfc6756bd6958a1ed8b9ad6
peetonn <peter@remap.ucla.edu>
2018-01-05 15:47:01 -0800

    [OpenFace] added training script

---
+++ edge/openface/train.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# 1st argument - folder with subfolders with face images
+# each subfolder name - person's name (or ID) which will be used as a label
+# for example, this is a possible folder structure for training:
+# 	- faces
+#		- john
+#			pic1.jpg
+#			pic2.jpg
+#			...
+#		- sam
+#			pic1.jpg
+#			pic2.jpg
+#			...
+#		- nick
+#			pic1.jpg
+#			pic2.jpg
+#			...
+
+FACES_FOLDER=$1
+
+echo "> processing faces from ${FACES_FOLDER}..."
+/openface/util/align-dlib.py $FACES_FOLDER outerEyesAndNose /aligned --verbose
+/openface/batch-represent/main.lua -outDir /reps -data /aligned
