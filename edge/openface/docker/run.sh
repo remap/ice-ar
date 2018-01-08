1681a791030fd83e8e4bd69059ef26dd7fa0dec6
peetonn <peter@remap.ucla.edu>
2018-01-08 14:03:58 -0800

    [OpenFace] added nanomsg patch, added run.sh; updated Dockerfile

---
+++ edge/openface/docker/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python /ice-ar/edge/openface/main.py \
+	--reps=$REPS --labels=$LABELS \
+	--torchmodel=$TORCH_MODEL --dlibmodel=$DLIB_MODEL \
+	--input=$INPUT --output=$OUTPUT --preview=$PREVIEW \
+	--frame-width=$FRAME_WIDTH --frame-height=$FRAME_HEIGHT
