44ff38aa84ce10096143d26f1df98e2c48fe728c
peetonn <peter@remap.ucla.edu>
2018-01-09 18:50:01 -0800

    [Consumer] updated scripts and Dockerfile

---
+++ edge/ndnrtc/docker/run-preview.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+while true; do
+	while [ -z "$SINK" ]; do
+		SINK=`cat $CONSUMER_CONFIG | gawk ' match($0, /sink.?=.*"(.*)";/, a) { print a[1] }'`
+		sleep 0.2
+	done;
+
+	while [ -z "$SINKFILE" ]; do
+		SINKFILE=`ls ${SINK}*`
+		sleep 0.5
+	done; 
+
+	/ice-ar/edge/misc/nanopipe-adaptor/nanopipe $SINKFILE /preview/mt-out
+done;
+
