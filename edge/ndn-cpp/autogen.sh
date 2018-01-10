33dba009c283a935e4dbd0a0100658516ad49fdf
peetonn <peter@remap.ucla.edu>
2018-01-09 20:44:07 -0800

    [Publisher] removed legacy code

--- edge/ndn-cpp/autogen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -e
-
-test -n "$srcdir" || srcdir=`dirname "$0"`
-test -n "$srcdir" || srcdir=.
-autoreconf --force --install --verbose "$srcdir"
-# Don't run ./configure since the user may want to run ./configure with options.
-echo "" ; echo "You need to run ./configure again."
