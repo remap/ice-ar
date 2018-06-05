330c1a2936cfae51fcbeb980ee22ada4d3f15545
peetonn <gpeetonn@gmail.com>
2018-06-05 13:36:42 -0700

    updated logging wrapper class

--- mobile/Mobile Terminal/Assets/Scripts/utils/Logging.cs
+++ mobile/Mobile Terminal/Assets/Scripts/utils/Logging.cs
@@ -1,6 +1,11 @@
 ﻿using UnityEngine;
 using System;
 
+public interface ILogComponent {
+    string getLogComponentName();
+    bool isLoggingEnabled();
+}
+
 public class Debug
 {
     static private string applicationTag_ = "ice-ar-app";
@@ -10,11 +15,35 @@ public class Debug
         applicationTag_ = applicationTag;
     }
 
+    #region Exception
+    public static void LogException(System.Exception exception)
+    {
+        ErrorFormat("exception occurred: {0}, stacktrace: {1}", 
+                    replaceNewLines(exception.Message),
+                    replaceNewLines(exception.StackTrace));
+        //UnityEngine.Debug.LogException(exception);
+    }
+
+    public static void LogException(object context, System.Exception exception)
+    {
+        ErrorFormat("exception occurred in {0}: {1}, stacktrace: {2}",
+                    context, replaceNewLines(exception.Message),
+                    replaceNewLines(exception.StackTrace));
+        //UnityEngine.Debug.LogException(exception, context);
+    }
+    #endregion
+
     #region Error
     public static void ErrorFormat(UnityEngine.Object context, string template, params object[] args)
     {
         var message = string.Format(template, args);
         LogError(context, message);
+    }
+
+    public static void ErrorFormat(ILogComponent component, string template, params object[] args)
+    {
+        var message = string.Format(template, args);
+        LogError(component, message);
     }
 
     public static void ErrorFormat(string template, params object[] args)
@@ -30,7 +59,18 @@ public class Debug
 
     public static void LogError(UnityEngine.Object context, object message)
     {
-        UnityEngine.Debug.LogError(String.Format("[{0}] {1}", applicationTag_, message), context);
+        if (context is ILogComponent && (context as ILogComponent).isLoggingEnabled())
+            UnityEngine.Debug.LogError(String.Format("[{0}][{1}] {2}", applicationTag_,
+                                                       (context as ILogComponent).getLogComponentName(), message));
+        else
+            UnityEngine.Debug.LogError(String.Format("[{0}] {1}", applicationTag_, message), context);
+    }
+
+    public static void LogError(ILogComponent component, object message)
+    {
+        if (component.isLoggingEnabled())
+            UnityEngine.Debug.LogError(String.Format("[{0}][{1}] {2}", applicationTag_,
+                                                       component.getLogComponentName(), message));
     }
     #endregion
 
@@ -39,6 +79,12 @@ public class Debug
     {
         var message = string.Format(template, args);
         LogWarning(context, message);
+    }
+
+    public static void WarningFormat(ILogComponent component, string template, params object[] args)
+    {
+        var message = string.Format(template, args);
+        LogWarning(component, message);
     }
 
     public static void WarningFormat(string template, params object[] args)
@@ -54,7 +100,18 @@ public class Debug
 
     public static void LogWarning(UnityEngine.Object context, object message)
     {
-        UnityEngine.Debug.LogWarning(String.Format("[{0}] {1}", applicationTag_, message), context);
+        if (context is ILogComponent && (context as ILogComponent).isLoggingEnabled())
+            UnityEngine.Debug.LogWarning(String.Format("[{0}][{1}] {2}", applicationTag_, 
+                                                       (context as ILogComponent).getLogComponentName(), message));
+        else
+            UnityEngine.Debug.LogWarning(String.Format("[{0}] {1}", applicationTag_, message), context);
+    }
+
+    public static void LogWarning(ILogComponent component, object message)
+    {
+        if (component.isLoggingEnabled())
+            UnityEngine.Debug.LogWarning(String.Format("[{0}][{1}] {2}", applicationTag_,
+                                                       component.getLogComponentName(), message));
     }
     #endregion
 
@@ -63,6 +120,12 @@ public class Debug
     {
         var message = string.Format(template, args);
         Message(context, message);
+    }
+
+    public static void MessageFormat(ILogComponent component, string template, params object[] args)
+    {
+        var message = string.Format(template, args);
+        Message(component, message);
     }
 
     public static void MessageFormat(string template, params object[] args)
@@ -78,7 +141,16 @@ public class Debug
 
     public static void Message(UnityEngine.Object context, object message)
     {
-        UnityEngine.Debug.LogFormat(context, "[{0}] {1}", applicationTag_, message);
+        if (context is ILogComponent && (context as ILogComponent).isLoggingEnabled())
+            UnityEngine.Debug.LogFormat("[{0}][{1}] {2}", applicationTag_, (context as ILogComponent).getLogComponentName(), message);
+        else
+            UnityEngine.Debug.LogFormat(context, "[{0}] {1}", applicationTag_, message);
+    }
+
+    public static void Message(ILogComponent component, object message)
+    {
+        if (component.isLoggingEnabled())
+            UnityEngine.Debug.LogFormat("[{0}][{1}] {2}", applicationTag_, component.getLogComponentName(), message);
     }
     #endregion
 
@@ -88,6 +160,12 @@ public class Debug
     {
         var message = string.Format(template, args);
         Log(context, message);
+    }
+
+    public static void LogFormat(ILogComponent component, string template, params object[] args)
+    {
+        var message = string.Format(template, args);
+        Log(component, message);
     }
 
     [System.Diagnostics.Conditional("DEVELOPMENT_BUILD")]
@@ -100,14 +178,27 @@ public class Debug
     [System.Diagnostics.Conditional("DEVELOPMENT_BUILD")]
     public static void Log(object message)
     {
-        UnityEngine.Debug.LogFormat("[{0}][TRACE] {1}", applicationTag_, message);
+        UnityEngine.Debug.LogFormat("[TRACE][{0}] {1}", applicationTag_, message);
     }
 
     [System.Diagnostics.Conditional("DEVELOPMENT_BUILD")]
     public static void Log(UnityEngine.Object context, object message)
     {
-        UnityEngine.Debug.LogFormat(context, "[{0}][TRACE] {1}", applicationTag_, message);
+        if (context is ILogComponent && (context as ILogComponent).isLoggingEnabled())
+            UnityEngine.Debug.LogFormat("[TRACE][{0}][{1}] {2}", applicationTag_, (context as ILogComponent).getLogComponentName(), message);
+        else
+            UnityEngine.Debug.LogFormat(context, "[TRACE][{0}] {1}", applicationTag_, message);
+    }
+
+    public static void Log(ILogComponent component, object message)
+    {
+        if (component.isLoggingEnabled())
+            UnityEngine.Debug.LogFormat("[TRACE][{0}][{1}] {2}", applicationTag_, component.getLogComponentName(), message);
     }
     #endregion
 
+    private static string replaceNewLines(string s)
+    {
+        return s.Replace(System.Environment.NewLine, System.Environment.NewLine + "[" + applicationTag_ + "]");
+    }
 }
