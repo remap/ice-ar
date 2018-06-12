a2eb85885541e3169df0384f9e46367d7421d2eb
Edward Lu <edwardzlu98@gmail.com>
2018-06-11 22:49:13 -0700

    Added ble integration

---
+++ mobile/Mobile Terminal/Assets/Scripts/NdnBleDisplayTextScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+using UnityEngine.UI;
+
+class NdnBleDisplayTextScript : MonoBehaviour
+{
+    public Text beaconListText;
+    public Text notificationText;
+    bool firstUpdateLoop = true;
+    Hashtable beaconsInRange;
+
+    IEnumerator showMessage ()
+    {
+        notificationText.enabled = true;
+        yield return new WaitForSeconds(5);
+        notificationText.enabled = false;
+    }
+
+    private void Start()
+    {
+        notificationText.enabled = false;
+        notificationText.text = "Beacons in range list was updated.";
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void updateBeaconListText(string message)
+    {
+        beaconListText.text = message;
+    }
+
+    public void notifyUserOfBeaconListChange()
+    {
+        IEnumerator messageDisplayCoroutine = showMessage();
+
+        StartCoroutine(messageDisplayCoroutine);
+    }
+}
